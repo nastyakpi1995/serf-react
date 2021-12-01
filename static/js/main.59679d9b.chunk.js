3c2d3cfe25c81b67801f3239ebd379e79931d803
Nastya Krat <nastyakpi1995@gmail.com>
2021-12-01 16:49:16 +0200

    Updates

--- static/js/main.59679d9b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpserf-react"]=this["webpackJsonpserf-react"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),a=c.n(s),i=(c(8),c(0));var d=function(){return Object(i.jsx)("div",{className:"App",children:"fsdfsdf"})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
-//# sourceMappingURL=main.59679d9b.chunk.js.map
